a8fd6681955d53ca99446f0e09b15ecb3121d574
hjyotisaikia11-debug <hjyotisaikia11@gmail.com>
2026-03-25 23:42:34 +0530

    bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,7 @@
+process.on('uncaughtException', (err) => {
+    console.log('Error:', err.message);
+});
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
